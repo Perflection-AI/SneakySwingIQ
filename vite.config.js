30ca7ddb1751f38de37aa59239f30b468404ed25
FranklinXu <flinkxuchuhan@gmail.com>
2026-03-20 17:55:23 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Replace 'swingiq-survey' with your actual GitHub repository name
 export default defineConfig({
   plugins: [react()],
-  base: '/swingiq-survey/',
+  base: '/SneakySwingIQ/',
 })
